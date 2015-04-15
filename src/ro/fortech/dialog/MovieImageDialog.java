ed9475060c5d03492a1b08091468248c1d0cf5b8
ioanafoidas <ioana.foidas@fortech.ro>
2015-04-15 12:48:00 +0300

    added movieImage page and displayImage button

--- src/ro/fortech/dialog/MovieImageDialog.java
+++ src/ro/fortech/dialog/MovieImageDialog.java
@@ -5,6 +5,8 @@ import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+import ro.fortech.model.Movie;
+
 
 @ManagedBean(name="movieImageDialog")
 @SessionScoped
@@ -13,6 +15,11 @@ public class MovieImageDialog implements Serializable{
 	
 	private static final long serialVersionUID = 8621173128034519586L;
     private String imagine;
+    
+    public String displayImage(String movieImage){
+    	
+    	return "movieImage?faces-redirect=true";
+    }
     
     
 	public String getImagine() {
