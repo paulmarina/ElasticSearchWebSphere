460514adfa7284fcdc44d1ed80f1ff39d2d9db7c
alinamoldovan90 <alina.moldovan@fortech.ro>
2015-04-21 10:52:52 +0300

    added file upload control on edit movie page

--- src/ro/fortech/business/ImageController.java
+++ src/ro/fortech/business/ImageController.java
@@ -1,11 +1,18 @@
 package ro.fortech.business;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import ro.fortech.access.ImageAccess;
+import ro.fortech.model.Image;
 
 @Stateless
 public class ImageController {
 	
+	@Inject
+	private ImageAccess imgAcc;
 	
-	
-
+	private List<Image> moviesList;
 }
